f3f1f8d2fe8c476a61e3fcbe4e28a826574de6c8
Matúš <xmachatam@stuba.sk>
2022-02-20 19:47:28 +0100

    uloha compare hotova

--- src/main/java/fei_oop/app.java
+++ src/main/java/fei_oop/app.java
@@ -3,20 +3,29 @@ package fei_oop;
 public class app {
     public static void main(String[] args) {
         //System.out.println("Yeet");
-        int x=9;
-        int y=6;
-        app.comp(x,y);
+        int x=5;
+        int y=5;
+        int rtn;
+        rtn = app.comp(x,y);
+        System.out.println(rtn);
     }
     public static int comp(int a, int b){
         //int a,b;
         //a = Integer.parseInt(args[0]);
         //b = Integer.parseInt(args[1]);
+        int rtn = 5;
         if (a>b) {
-            System.out.println("cislo" + a + "je vacsie ako cislo" + b);
-            return 1;
+            System.out.println("cislo " + a + "je vacsie ako cislo" + b);
+            rtn = 1;
         }
-        if (a==b) return 0;
-        if (a<b) return -1;
-        return 5;
+        if (a==b) {
+            System.out.println("cisla su rovnake");
+            rtn = 0;
+        }
+        if (a<b) {
+            System.out.println("cislo " + a + "je mensie ako cislo" + b);
+            rtn = -1;
+        }
+        return rtn;
     }
 }
