4efcdf4a237b24a2871e41e67fe5fb001b0c6348
Matúš <xmachatam@stuba.sk>
2022-02-20 19:37:25 +0100

    println test

--- src/main/java/fei_oop/app.java
+++ src/main/java/fei_oop/app.java
@@ -3,7 +3,7 @@ package fei_oop;
 public class app {
     public static void main(String[] args) {
         //System.out.println("Yeet");
-        int x=5;
+        int x=9;
         int y=6;
         app.comp(x,y);
     }
@@ -11,7 +11,10 @@ public class app {
         //int a,b;
         //a = Integer.parseInt(args[0]);
         //b = Integer.parseInt(args[1]);
-        if (a>b) return 1;
+        if (a>b) {
+            System.out.println("cislo" + a + "je vacsie ako cislo" + b);
+            return 1;
+        }
         if (a==b) return 0;
         if (a<b) return -1;
         return 5;
