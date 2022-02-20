87ac2760be161cfc51668919cddbad3e3f76b45c
Matúš <xmachatam@stuba.sk>
2022-02-20 19:31:02 +0100

    comp method

--- src/main/java/fei_oop/app.java
+++ src/main/java/fei_oop/app.java
@@ -2,6 +2,18 @@ package fei_oop;
 
 public class app {
     public static void main(String[] args) {
-        System.out.println("Yeet");
+        //System.out.println("Yeet");
+        int x=5;
+        int y=6;
+        app.comp(x,y);
+    }
+    public static int comp(int a, int b){
+        //int a,b;
+        //a = Integer.parseInt(args[0]);
+        //b = Integer.parseInt(args[1]);
+        if (a>b) return 1;
+        if (a==b) return 0;
+        if (a<b) return -1;
+        return 5;
     }
 }
